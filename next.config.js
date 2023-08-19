db959f55c159c021ca320a49f681646af72e613a
Lucas Josino <contact@lucasjosino.com>
2023-08-19 10:48:46 -0300

    feat: Add initial configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's6.imgcdn.dev',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
