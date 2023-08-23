cbf629ecb30653acddbbdb33f983b2935971dbb1
Lucas Josino <contact@lucasjosino.com>
2023-08-23 12:31:44 -0300

    feat: Update details layout and Add details fallback

---
+++ src/components/templates/DetailsFallback.tsx
@@ -0,0 +1,30 @@
+import styles from '@/styles/DetailsFallback.module.css';
+import { useRouter } from 'next/router';
+import BaseLink from '../core/BaseLink';
+import PrimaryButton from '../core/PrimaryButton';
+
+export default function DetailsFallback() {
+  const router = useRouter();
+  const report = {
+    url: 'https://github.com/LucJosin/hawbrary/issues/new',
+    title: `${encodeURI('[Explorer]: The requested URL was not found')}`,
+    assignees: 'LucJosin',
+    labels: 'bug',
+    body: `${encodeURI(`Path: ${router.asPath}`)}`,
+  };
+  return (
+    <div className={styles.fallback}>
+      <h2>The requested URL was not found</h2>
+      <span className={styles.options}>
+        <PrimaryButton href="/explore" isLocal={true} name="Go to explorer" />
+      </span>
+      <span className={styles.report}>
+        Something is wrong?{' '}
+        <BaseLink
+          href={`${report.url}?title=${report.title}&assignees=${report.assignees}&labels=${report.labels}&body=${report.body}`}
+          name="Report error"
+        />
+      </span>
+    </div>
+  );
+}
--- src/layout/DetailsPage.tsx
+++ src/layout/DetailsPage.tsx
@@ -1,15 +1,24 @@
+import DetailsFallback from '@/components/templates/DetailsFallback';
 import styles from '@/styles/DetailsPage.module.css';
 import type { ReactNode } from 'react';
 import Layout from './Layout';
 
 interface Props {
   children: ReactNode;
+  isLoading?: boolean;
+  hasData?: boolean;
 }
 
-export default function DetailsPage({ children }: Props) {
+export default function DetailsPage({
+  children,
+  isLoading,
+  hasData = false,
+}: Props) {
   return (
     <Layout isCentered={true}>
-      <span className={styles.container}>{children}</span>
+      {isLoading && <div className={styles.loading}></div>}
+      {hasData && <span className={styles.container}>{children}</span>}
+      {!isLoading && !hasData && <DetailsFallback />}
     </Layout>
   );
 }
