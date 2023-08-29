70d184836b93a795afac884442cb5b2e53c386a0
Lucas Josino <contact@lucasjosino.com>
2023-08-29 12:28:46 -0300

    fix: Wrong header links and scroll

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -18,30 +18,26 @@ export default function Header() {
       <Link href="/">
         <TextEffect title="Hawbrary" size="1.2rem" />
       </Link>
-      {router.asPath.includes('/explore') && (
-        <>
-          <div className={styles.links}>
-            <Link href="#actors">Actors</Link>
-            <Link href="#characters">Characters</Link>
-            <Link href="#episodes">Episodes</Link>
-            <Link href="#games">Games</Link>
-            <Link href="#locations">Locations</Link>
-          </div>
-          <div className={styles.selection}>
-            <select
-              name="languages"
-              id="languages"
-              className={styles.select}
-              onChange={updateLanguage}
-              defaultValue="en-US"
-            >
-              <option value="en-US">en-US</option>
-              <option value="pt-BR">pt-BR</option>
-            </select>
-            <Icon icon="tabler:language" width="20" />
-          </div>
-        </>
-      )}
+      <div className={styles.links}>
+        <Link href="/explore/#actors">Actors</Link>
+        <Link href="/explore/#characters">Characters</Link>
+        <Link href="/explore/#episodes">Episodes</Link>
+        <Link href="/explore/#seasons">Seasons</Link>
+        <Link href="/explore/#locations">Locations</Link>
+      </div>
+      <div className={styles.selection}>
+        <select
+          name="languages"
+          id="languages"
+          className={styles.select}
+          onChange={updateLanguage}
+          defaultValue="en-US"
+        >
+          <option value="en-US">en-US</option>
+          <option value="pt-BR">pt-BR</option>
+        </select>
+        <Icon icon="tabler:language" width="20" />
+      </div>
     </header>
   );
 }
