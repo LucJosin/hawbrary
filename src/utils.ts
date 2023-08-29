9d9a1eda2ff26cf5ac53fa8a5d12d2d9b1e057c4
Lucas Josino <contact@lucasjosino.com>
2023-08-28 22:07:58 -0300

    feat: Add new utils functions

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,12 @@ export function getUuidFromHref(href: string): string | undefined {
   const split = href.split('/');
   return split.pop();
 }
+
+export function getDetailsUrlFromHref(target: string, href: string): string {
+  const uuid = getUuidFromHref(href);
+  return buildDetailsUrl(target, uuid ?? '');
+}
+
+export function buildDetailsUrl(target: string, uuid: string): string {
+  return `/explore/${target}/details/uuid=${uuid}`;
+}
