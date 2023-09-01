c228cc5a0985cb4d043b9625262d5d96c5ee9dc0
Lucas Josino <contact@lucasjosino.com>
2023-08-31 21:35:22 -0300

    feat: Add initial pagination for episodes

--- src/pages/explore/episodes/index.tsx
+++ src/pages/explore/episodes/index.tsx
@@ -1,30 +1,56 @@
 import { Card } from '@/components/core/Card';
+import { Pagination } from '@/components/core/Pagination';
+import { Title } from '@/components/core/Title';
 import { Fallback } from '@/components/templates/Fallback';
+import Grid from '@/components/templates/Grid';
 import { getAllEpisodes } from '@/lib/hawapi';
-import styles from '@/styles/Items.module.css';
+import styles from '@/styles/MorePage.module.css';
+import { useState } from 'react';
 import useSWR from 'swr';
 
-export default function Items() {
-  const { data, error, isLoading } = useSWR('episodes', getAllEpisodes);
+export default function MorePage() {
+  const [page, setPage] = useState(1);
+
+  const { data, error, isLoading } = useSWR(`actors?page=${page}`, () => {
+    return getAllEpisodes({ page });
+  });
 
   return (
     <Fallback.Layout
       isLoading={isLoading}
       hasData={!(error || data?.status !== 200)}
     >
-      <div className={styles.items}>
-        {data?.data?.map((item, key) => {
-          return (
-            <Card.Simple
-              key={key}
-              uuid={item.uuid}
-              target="episodes"
-              title={item.title}
-              description={item.description}
-              thumbnail={item.thumbnail}
-            />
-          );
-        })}
+      <div className={styles.container}>
+        <Title.Simple text="Episodes" />
+        <Grid>
+          {data?.data?.map((item, key) => {
+            return (
+              <Card.Simple
+                key={key}
+                uuid={item.uuid}
+                target="episodes"
+                title={`${item.title} - Ep.${item.episode_num}`}
+                description={item.description}
+                thumbnail={item.thumbnail}
+              />
+            );
+          })}
+        </Grid>
+        <Pagination.Root>
+          {data?.prev_page && (
+            <Pagination.Page name="Prev" onClick={() => setPage(page - 1)} />
+          )}
+          <Pagination.Page name={`${data?.page}`} isSelected={true} />
+          {data?.page_total !== page && (
+            <>
+              <span>...</span>
+              <Pagination.Page name={`${data?.page_total}`} />
+            </>
+          )}
+          {data?.next_page && data?.page_total !== page && (
+            <Pagination.Page name="Next" onClick={() => setPage(page + 1)} />
+          )}
+        </Pagination.Root>
       </div>
     </Fallback.Layout>
   );
