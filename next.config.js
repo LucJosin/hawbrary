1dc921a79b20f390d583b7d6087978f152672835
Lucas Josino <contact@lucasjosino.com>
2023-09-08 22:08:49 -0300

    feat: Only show css names as hash on production

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const loaderUtils = require('loader-utils');
+
+const hashOnlyIdent = (context, _, exportName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, '/')}#className:${exportName}`
+      ),
+      'md4',
+      'base64',
+      6
+    )
+    .replace(/[^a-zA-Z0-9-_]/g, '_')
+    .replace(/^(-?\d|--)/, '_$1');
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
@@ -6,6 +25,24 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  webpack(config, { dev }) {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    if (!dev)
+      rules.forEach((rule) => {
+        rule.use.forEach((moduleLoader) => {
+          if (
+            moduleLoader.loader?.includes('css-loader') &&
+            !moduleLoader.loader?.includes('postcss-loader')
+          )
+            moduleLoader.options.modules.getLocalIdent = hashOnlyIdent;
+        });
+      });
+
+    return config;
   },
 };
 
